bdd01fa28c0fb20e3dac8d0281c9bb60230499f2
Tiri Tuatai <tiri.jnr@gmail.com>
2024-02-19 01:24:34 +0800

    Functions Lab cont Done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return (`This Saturday, I want to ${activity}!`);
+}
+ 
+saturdayFun();
+
+const mondayWork = function(todo = "go to the office") {
+    return (`This Monday, I will ${todo}.`);
+}
+
+mondayWork();
+
+function wrapAdjective (phrase1) {
+    return function inner(phrase2 = "special") {
+        return `You are ${phrase1}${phrase2}${phrase1}!`;
+    } 
+}
+
+
